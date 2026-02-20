73553a2b8bc837bbb4cec42ee01a1f2a527a3cb9
Matheus Alves de Freitas <matheusalvesdefreitas66616@gmail.com>
2026-02-20 17:11:40 +0100

    Configure trusted proxies for HTTPS in production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
@@ -24,4 +25,4 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })->trustHosts(at: '*')->create();
